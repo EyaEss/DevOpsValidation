4c7cdcdac3d6875cc29ab704c81ecd1487f2de20
EyaEss <61499019+EyaEss@users.noreply.github.com>
2024-03-30 00:29:48 +0100

    Update ActivitySectorImplTest.java

--- src/test/java/tn/esprit/devops_project/services/ActivitySectorImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/ActivitySectorImplTest.java
@@ -28,5 +28,73 @@ class ActivitySectorImplTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
     }
+    @Test
+    public void testRetrieveAllActivitySectors() {
+        // Given
+        ActivitySector activitySector1 = new ActivitySector("Sector 1");
+        ActivitySector activitySector2 = new ActivitySector("Sector 2");
+        List<ActivitySector> activitySectors = Arrays.asList(activitySector1, activitySector2);
+        when(activitySectorRepository.findAll()).thenReturn(activitySectors);
+
+        // When
+        List<ActivitySector> result = activitySectorService.retrieveAllActivitySectors();
+
+        // Then
+        assertEquals(2, result.size());
+        assertEquals("Sector 1", result.get(0).getName());
+        assertEquals("Sector 2", result.get(1).getName());
+    }
+
+    @Test
+    public void testAddActivitySector() {
+        // Given
+        ActivitySector activitySector = new ActivitySector("New Sector");
+        when(activitySectorRepository.save(activitySector)).thenReturn(activitySector);
+
+        // When
+        ActivitySector result = activitySectorService.addActivitySector(activitySector);
+
+        // Then
+        assertEquals("New Sector", result.getName());
+    }
+
+    @Test
+    public void testDeleteActivitySector() {
+        // Given
+        Long id = 1L;
+
+        // When
+        activitySectorService.deleteActivitySector(id);
+
+        // Then
+        verify(activitySectorRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    public void testUpdateActivitySector() {
+        // Given
+        ActivitySector activitySector = new ActivitySector("Updated Sector");
+        when(activitySectorRepository.save(activitySector)).thenReturn(activitySector);
+
+        // When
+        ActivitySector result = activitySectorService.updateActivitySector(activitySector);
+
+        // Then
+        assertEquals("Updated Sector", result.getName());
+    }
+
+    @Test
+    public void testRetrieveActivitySector() {
+        // Given
+        Long id = 1L;
+        ActivitySector activitySector = new ActivitySector("Sector");
+        when(activitySectorRepository.findById(id)).thenReturn(Optional.of(activitySector));
+
+        // When
+        ActivitySector result = activitySectorService.retrieveActivitySector(id);
+
+        // Then
+        assertEquals("Sector", result.getName());
+    }
  
 }
